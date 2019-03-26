f66448a1f1c0c049240a34571950e5a8814a0639
Vangelis Katsikaros <vangelis@adzuna.com>
2019-03-26 10:39:15 +0200

    Improve twitter script
    * Move data from hardcoded hashes to files
    * Convert data stings from unicode named code point to actual characters
      for easier editing
    * Add latex output
    
    TODO: The latex output is neither an ideal output or in an ideal state.
    There is no templating to separate logic and presentation.

--- scripts/twitter_top_words.pl
+++ scripts/twitter_top_words.pl
@@ -1,11 +1,14 @@
 #!/usr/bin/env perl
 use strict;
 use warnings;
+use utf8;
+use Data::Dumper;
 use feature 'say';
 binmode(STDOUT, ":encoding(UTF-8)");
 use Getopt::Long;
 
 use Net::Twitter;
+use File::Slurper 'read_text';
 
 my $consumer_key;
 my $consumer_secret;
@@ -23,53 +26,20 @@ my $result = GetOptions(
     'latex'                  => \$opt_latex,
     'stats'                  => \$opt_stats,
 );
+my $count_threshold = 8;
 my @accounts = split(/,/,$opt_accounts);
 
-my %stopwords = (
-    "\N{ARABIC LETTER FEH}\N{ARABIC LETTER YEH}" => 1, # fi
-    "\N{ARABIC LETTER MEEM}\N{ARABIC LETTER NOON}" => 1, # min, man, ...
-    "\N{ARABIC LETTER AIN}\N{ARABIC LETTER LAM}\N{ARABIC LETTER ALEF MAKSURA}" => 1, # Ala
-    "\N{ARABIC LETTER ALEF WITH HAMZA ABOVE}\N{ARABIC LETTER NOON}" => 1, # an
-    "\N{ARABIC LETTER ALEF WITH HAMZA BELOW}\N{ARABIC LETTER LAM}\N{ARABIC LETTER ALEF MAKSURA}" => 1, # ila
-    "\N{ARABIC LETTER MEEM}\N{ARABIC LETTER AIN}" => 1, # mA
-    "\N{ARABIC LETTER AIN}\N{ARABIC LETTER NOON}" => 1, # An
-    "\N{ARABIC LETTER BEH}\N{ARABIC LETTER YEH}" => 1, # bi
-    "\N{ARABIC LETTER MEEM}\N{ARABIC LETTER ALEF}" => 1, # ma
-    "\N{ARABIC LETTER LAM}\N{ARABIC LETTER ALEF}" => 1, # la
-    "\N{ARABIC LETTER HEH}\N{ARABIC LETTER LAM}" => 1, # hal
-    "\N{ARABIC LETTER HEH}\N{ARABIC LETTER YEH}" => 1, # hia
-    "\N{ARABIC LETTER ALEF}\N{ARABIC LETTER LAM}\N{ARABIC LETTER TEH}\N{ARABIC LETTER YEH}" => 1, # alati
-    "\N{ARABIC LETTER ALEF}\N{ARABIC LETTER LAM}\N{ARABIC LETTER THAL}\N{ARABIC LETTER YEH}" => 1, # aladi
-    "\N{ARABIC LETTER BEH}\N{ARABIC LETTER AIN}\N{ARABIC LETTER DAL}" => 1, # baad
-    "\N{ARABIC LETTER NOON}\N{ARABIC LETTER HAH}\N{ARABIC LETTER WAW}" => 1, #nahou
-    "\N{ARABIC LETTER WAW}" => 1, #ou
-);
-my $nt = Net::Twitter->new(
-    traits   => [ qw/ API::RESTv1_1 OAuth / ],
-    consumer_key        => $consumer_key,
-    consumer_secret     => $consumer_secret,
-    access_token        => $access_token,
-    access_token_secret => $access_token_secret,
-    ssl => 1
-);
+my $stopwords =  read_word_lists('stopwords');
+my $location_names = read_word_lists('location_names');
+my $people_names = read_word_lists('people_names');
+#print Dumper($location_names); exit;
+my $tweets = get_tweets();
 
-my @tweets;
-foreach my $account ( @accounts ) {
-    my $search_hash = {
-        screen_name => $account,
-        include_rts => 0,
-        count => 300,
-    };
-    my $r = $nt->user_timeline( $search_hash );
-    foreach my $status ( @$r ) {
-	push @tweets, $status->{text};
-    }
-}
+# "\N{ARABIC LETTER ALEF}\N{ARABIC LETTER LAM}\N{ARABIC LETTER TEH}\N{ARABIC LETTER YEH}" => 1, # alati
 
 my %words;
-my $word_count;
-my $stopword_count;
-foreach my $t ( @tweets ) {
+my %count;
+foreach my $t ( @$tweets ) {
     # compact whitespace
     $t =~ s/\s+/ /g;
     $t =~ s/^\s+//g;
@@ -89,23 +59,52 @@ foreach my $t ( @tweets ) {
       	next if length($i) == 0; # ignore empty words
         next if $i =~ m/^\W+$/; # ignore non-"word" characters only words
         # ignore common words
-        if( $stopwords{$i} ) {
-            $stopword_count++;
+        $count{total}++;
+        if( $stopwords->{$i} ) {
+            $count{stopword}++;
             next;
         }
+        if( $location_names->{$i} ) {
+          $count{location}++;
+          next;
+        }
+        if( $people_names->{$i} ) {
+          $count{people}++;
+          next;
+        }
         $words{$i}++;
-        $word_count++;
+        $count{word}++;
     }
 }
+
+my $over_threshold_count;
+my $over_threshold_distinct;
+foreach my $w ( sort { $words{$a} <=> $words{$b} } keys %words ) {
+  if( $words{$w} >= $count_threshold ) {
+    $over_threshold_count += $words{$w};
+    $over_threshold_distinct++;
+  }
+}
+
+if( $opt_latex ) {
+  say read_text('templates/start_all.tex');
+}
+say "\\footnotetext[1]{" . scalar(@$tweets). " tweets. Word occurences $count{total}: $count{word} interesting words + $count{stopword} stopwords + $count{location} locations + $count{people} people}";
+say "\\footnotetext[2]{This document includes $over_threshold_distinct distinct words ($over_threshold_count occurences) }";
+
 my %stats;
 foreach my $w ( sort { $words{$a} <=> $words{$b} } keys %words ) {
     $stats{$words{$w}}++;
     if( $opt_stats ) {
       say $words{$w} . ' ' .$w;
     }
-    if( $opt_latex && $words{$w} >= 8 ) {
+    if( $opt_latex && $words{$w} >= $count_threshold ) {
       say "$words{$w} & & \\ar{ $w } \\\\";
     }
+}
+
+if( $opt_latex ) {
+  say read_text('templates/end_all.tex');
 }
 
 if( $opt_stats ) {
@@ -113,7 +112,46 @@ if( $opt_stats ) {
   foreach my $i ( sort {$a <=> $b} keys %stats ) {
       say "$i $stats{$i}";
   }
-  say scalar(@tweets)." tweets, $word_count words + $stopword_count stopwords";
+}
+
+sub read_word_lists
+{
+  my ($file) = @_;
+  my %words;
+  my $txt = read_text("data/$file.txt");
+  my @lines = split(/\n/, $txt);
+  foreach my $line (@lines) {
+    my @tokens = split(/\,/, $line);
+    #print Dumper(\@tokens);
+    $words{ $tokens[0] } = 1;
+  }
+  return \%words;
+}
+
+sub get_tweets
+{
+  my $nt = Net::Twitter->new(
+      traits   => [ qw/ API::RESTv1_1 OAuth / ],
+      consumer_key        => $consumer_key,
+      consumer_secret     => $consumer_secret,
+      access_token        => $access_token,
+      access_token_secret => $access_token_secret,
+      ssl => 1
+  );
+
+  my @tweets;
+  foreach my $account ( @accounts ) {
+      my $search_hash = {
+          screen_name => $account,
+          include_rts => 0,
+          count => 300,
+      };
+      my $r = $nt->user_timeline( $search_hash );
+      foreach my $status ( @$r ) {
+        push @tweets, $status->{text};
+      }
+  }
+  return \@tweets;
 }
 
 exit(0);

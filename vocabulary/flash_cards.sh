ac4aed7e6581d64674876a6827731acab86ef7b5
Vangelis Katsikaros <vkatsikaros@gmail.com>
2022-09-24 13:25:48 +0300

    Flash cards

--- vocabulary/flash_cards.sh
+++ vocabulary/flash_cards.sh
@@ -1,22 +1,28 @@
 #!/usr/bin/env bash
 
 # page 1
-convert -density 400 my_vocabulary_duo.pdf[0] -crop  625x3625+$((0*625+400))+401 -quality 99 flash_cards/page0_col0.jpg
-convert -density 400 my_vocabulary_duo.pdf[0] -crop  625x3625+$((1*625+400))+401 -quality 99 flash_cards/page1_col3.jpg
-convert -density 400 my_vocabulary_duo.pdf[0] -crop  625x3625+$((2*625+430))+401 -quality 99 flash_cards/page0_col1.jpg
-convert -density 400 my_vocabulary_duo.pdf[0] -crop  625x3625+$((3*625+400))+401 -quality 99 flash_cards/page1_col2.jpg
+convert -density 400 my_vocabulary_duo.pdf[0] -crop  625x3675+$((0*625+400))+401 -quality 99 flash_cards/page0_col0.jpg
+convert -density 400 my_vocabulary_duo.pdf[0] -crop  625x3675+$((1*625+400))+401 -quality 99 flash_cards/page1_col3.jpg
+convert -density 400 my_vocabulary_duo.pdf[0] -crop  625x3675+$((2*625+430))+401 -quality 99 flash_cards/page0_col1.jpg
+convert -density 400 my_vocabulary_duo.pdf[0] -crop  625x3675+$((3*625+400))+401 -quality 99 flash_cards/page1_col2.jpg
 
-convert -density 400 my_vocabulary_duo.pdf[1] -crop  625x3625+$((0*625+400))+401 -quality 99 flash_cards/page0_col2.jpg
-convert -density 400 my_vocabulary_duo.pdf[1] -crop  625x3625+$((1*625+400))+401 -quality 99 flash_cards/page1_col1.jpg
-convert -density 400 my_vocabulary_duo.pdf[1] -crop  625x3625+$((2*625+430))+401 -quality 99 flash_cards/page0_col3.jpg
-convert -density 400 my_vocabulary_duo.pdf[1] -crop  625x3625+$((3*625+400))+401 -quality 99 flash_cards/page1_col0.jpg
+convert -density 400 my_vocabulary_duo.pdf[1] -crop  625x3675+$((0*625+400))+401 -quality 99 flash_cards/page0_col2.jpg
+convert -density 400 my_vocabulary_duo.pdf[1] -crop  625x3675+$((1*625+400))+401 -quality 99 flash_cards/page1_col1.jpg
+convert -density 400 my_vocabulary_duo.pdf[1] -crop  625x3675+$((2*625+430))+401 -quality 99 flash_cards/page0_col3.jpg
+convert -density 400 my_vocabulary_duo.pdf[1] -crop  625x3675+$((3*625+400))+401 -quality 99 flash_cards/page1_col0.jpg
 # page 1 end
 
-# special case
-convert -density 400 my_vocabulary_duo.pdf[2] -crop  625x3625+$((0*625+400))+401 -quality 99 flash_cards/page2_col0.jpg
-convert -density 400 my_vocabulary_duo.pdf[2] -crop  625x3625+$((1*625+400))+401 -quality 99 flash_cards/page3_col3.jpg
-convert -density 400 my_vocabulary_duo.pdf[2] -crop  625x3625+$((2*625+430))+401 -quality 99 flash_cards/page2_col1.jpg
-convert -density 400 my_vocabulary_duo.pdf[2] -crop  625x3625+$((3*625+400))+401 -quality 99 flash_cards/page3_col2.jpg
+# page 2
+convert -density 400 my_vocabulary_duo.pdf[2] -crop  625x3675+$((0*625+400))+401 -quality 99 flash_cards/page2_col0.jpg
+convert -density 400 my_vocabulary_duo.pdf[2] -crop  625x3675+$((1*625+400))+401 -quality 99 flash_cards/page3_col3.jpg
+convert -density 400 my_vocabulary_duo.pdf[2] -crop  625x3675+$((2*625+430))+401 -quality 99 flash_cards/page2_col1.jpg
+convert -density 400 my_vocabulary_duo.pdf[2] -crop  625x3675+$((3*625+400))+401 -quality 99 flash_cards/page3_col2.jpg
+
+convert -density 400 my_vocabulary_duo.pdf[3] -crop  625x3675+$((0*625+400))+401 -quality 99 flash_cards/page2_col2.jpg
+convert -density 400 my_vocabulary_duo.pdf[3] -crop  625x3675+$((1*625+400))+401 -quality 99 flash_cards/page3_col1.jpg
+convert -density 400 my_vocabulary_duo.pdf[3] -crop  625x3675+$((2*625+430))+401 -quality 99 flash_cards/page2_col3.jpg
+convert -density 400 my_vocabulary_duo.pdf[3] -crop  625x3675+$((3*625+400))+401 -quality 99 flash_cards/page3_col0.jpg
+# page 2 end
 
 convert +append flash_cards/page0*.jpg flash_cards/s1.jpg
 convert +append flash_cards/page1*.jpg flash_cards/s2.jpg
